eb60d5aa81f9677cb7c327bf9f2f07efd10ee3f6
Oleg Koretsky <test.radiokot@gmail.com>
2021-03-27 21:05:39 +0200

    Add main method to run as a jar

--- src/main/kotlin/ua/com/radiokot/arduino2tcx/Main.kt
+++ src/main/kotlin/ua/com/radiokot/arduino2tcx/Main.kt
@@ -1,8 +1,42 @@
+@file:JvmName("Main")
+
 package ua.com.radiokot.arduino2tcx
 
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.attribute.BasicFileAttributes
+import java.text.SimpleDateFormat
+import java.util.*
+
 object Main {
+    init {
+        Locale.setDefault(Locale.ENGLISH)
+    }
+
     @JvmStatic
     fun main(args: Array<String>) {
+        if (args.isEmpty()) {
+            System.err.println("Expected log file path as the only argument")
+            return
+        }
 
+        val inputFile = File(args.first())
+        val inputFileAttributes = Files.readAttributes(inputFile.toPath(), BasicFileAttributes::class.java)
+
+        val points = ArduinoRecordLogParser(
+            timeFormat = SimpleDateFormat("HH:mm:ss.SSS"),
+            valueDelimiters = charArrayOf(' ', ';'),
+        )
+            .parseLog(
+                logDate = Date(inputFileAttributes.creationTime().toMillis()),
+                logReader = inputFile.bufferedReader(Charsets.UTF_8)
+            )
+
+        val outputFile = File("./output.tcx")
+
+        TcxFileRenderer().render(
+            points = points,
+            outputWriter = outputFile.bufferedWriter(Charsets.UTF_8)
+        )
     }
 }
